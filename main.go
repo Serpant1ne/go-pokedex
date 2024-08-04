07b5e20a5deab7e3f91cbd20b23553a74f30331b
Serpant1ne <gartsmankonstantin@gmail.com>
2024-08-04 16:15:18 +0200

    added todo

--- main.go
+++ main.go
@@ -3,3 +3,14 @@ package main
 func main() {
 	startRepl()
 }
+
+// TODO Update the CLI to support the "up" arrow to cycle through previous commands
+// TODO Simulate battles between pokemon
+// TODO Add more unit tests
+// TODO Refactor your code to organize it better and make it more testable
+// TODO Keep pokemon in a "party" and allow them to level up
+// TODO Allow for pokemon that are caught to evolve after a set amount of time
+// TODO Persist a user's Pokedex to disk so they can save progress between sessions
+// TODO Use the PokeAPI to make exploration more interesting. For example, rather than typing the names of areas, maybe you are given choices of areas and just type "left" or "right"
+// TODO Random encounters with wild pokemon
+// TODO Adding support for different types of balls (Pokeballs, Great Balls, Ultra Balls, etc), which have different chances of catching pokemon
